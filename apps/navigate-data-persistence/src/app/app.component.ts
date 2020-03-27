d2e6ffc8110150f9fa677e7a27c45d6af47bc91c
Maxim Kokosha <kokosha.maxim@gmail.com>
2020-03-27 19:08:02 +0200

    Initial commit

--- apps/navigate-data-persistence/src/app/app.component.ts
+++ apps/navigate-data-persistence/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UsersFacade } from './+state/users.facade';
 
 @Component({
   selector: 'navigate-data-persistence-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'navigate-data-persistence';
+
+  constructor(private usersFacade: UsersFacade) {}
+
+  ngOnInit() {
+    this.usersFacade.getUsers();
+  }
 }
--- apps/navigate-data-persistence/src/app/app.module.ts
+++ apps/navigate-data-persistence/src/app/app.module.ts
@@ -1,12 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  DefaultRouterStateSerializer,
+  NavigationActionTiming,
+  RouterState,
+  StoreRouterConnectingModule
+} from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NxModule } from '@nrwl/angular';
 
+import { environment } from '../environments/environment';
+import { UsersEffects } from './+state/users.effects';
+import { UsersFacade } from './+state/users.facade';
+import * as fromUsers from './+state/users.reducer';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { reducer } from './+state/users.reducer';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    NxModule.forRoot(),
+    StoreModule.forRoot(
+      { users: reducer },
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: false,
+          strictStateImmutability: false
+        }
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot({
+      navigationActionTiming: NavigationActionTiming.PostActivation,
+      serializer: DefaultRouterStateSerializer,
+      routerState: RouterState.Full
+    }),
+    EffectsModule.forRoot([UsersEffects])
+  ],
+  providers: [UsersFacade],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
